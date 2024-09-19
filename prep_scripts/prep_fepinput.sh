e885f438fa0c5207fe2b0e4231e4708e411a20f5
nayanikadas <nayanikadas0709@gmail.com>
2024-09-19 15:13:19 +0200

    commit chaanges

---
+++ prep_scripts/prep_fepinput.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# List of directories to process
+directories="/home/hp/nayanika/github/PhD_Thesis/EVB/protein_stepwise/GPX6MUT/individual_mutants/mousecys/1-prep/47minim
+/home/hp/nayanika/github/PhD_Thesis/EVB/protein_stepwise/GPX6MUT/individual_mutants/mousecys/1-prep/48minim
+/home/hp/nayanika/github/PhD_Thesis/EVB/protein_stepwise/GPX6MUT/individual_mutants/mousecys/1-prep/52minim
+/home/hp/nayanika/github/PhD_Thesis/EVB/protein_stepwise/GPX6MUT/individual_mutants/mousecys/1-prep/54minim
+/home/hp/nayanika/github/PhD_Thesis/EVB/protein_stepwise/GPX6MUT/individual_mutants/mousecys/1-prep/99minim"
+
+# Base directory where you want to create folders
+base_scr_dir="/home/hp/results"
+
+# Loop through each directory and process .pdb files
+for dir in $directories
+do
+    # Construct the system name based on the directory name
+    system_name=$(basename "$dir")
+
+    # Create directory for the system in $base_scr_dir
+    mkdir -p "$base_scr_dir/$system_name"
+
+    # Remove any existing replicas for a clean start
+    rm -rf "$base_scr_dir/$system_name/replica*"
+
+    # Find the .pdb file inside the current directory
+    for pdb_file in "$dir"/*.pdb
+    do
+        if [ -f "$pdb_file" ]; then
+            # Run q_genfeps.py with the system's pdb file and other input files from the current directory
+            q_genfeps.py "$dir/genfeps.proc" --pdb "$pdb_file" "$dir/relax_012.inp" relax --repeats 100 --frames 51 --fromlambda 1.0 --prefix "$base_scr_dir/$system_name/replica" --rs "$dir/run_qdyn_5.sh"
+        else
+            echo "No .pdb file found in $dir"
+        fi
+    done
+done
