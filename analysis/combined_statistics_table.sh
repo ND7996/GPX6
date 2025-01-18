e785dbb0d9dce127561052b55b2dd56758b7909f
nayanikadas <nayanikadas0709@gmail.com>
2025-01-18 19:13:23 +0100

    all

--- analysis/combined_statistics_table.sh
+++ analysis/combined_statistics_table.sh
@@ -3,9 +3,9 @@
 # Directory containing the stats file
 DATA_DIR="/home/hp/nayanika/github/GPX6/analysis"
 # File containing the existing tables
-STATS_FILE="$DATA_DIR/stats2.tex"
+STATS_FILE="$DATA_DIR/statsmouse3.tex"
 # File where the combined table will be stored
-TABLE_FILE="/home/hp/nayanika/github/GPX6/table/Free_Energy2.tex"
+TABLE_FILE="/home/hp/nayanika/github/GPX6/table/Free_Energy3.tex"
 
 # Start writing the complete LaTeX document to the output file
 {
@@ -26,8 +26,8 @@ while IFS= read -r line; do
     # Clean up the line and substitute +- with the proper LaTeX \pm
     clean_line=$(echo "$line" | tr -d '\r' | sed 's/\+-/\\pm/g')
 
-    # Check if the line contains data (includes keywords like WT, S47A, F48Y, T52A, T54Q, R99C, Cys, Sec, Mouse, Human, etc.)
-    if echo "$clean_line" | grep -q "WT\|S47A\|F48Y\|T52A\|T54Q\|R99C\|Cys\|Sec\|Mouse\|Human"; then
+    # Check if the line contains data (this can be customized if needed)
+    if echo "$clean_line" | grep -q -E "C49U|T52A|G102S|P142S|H177Q|T54Q|S47A|K87T|N107S|S4R|D148E|R99C|E143S|F48Y|T178T|H144Q|Y104F|R181S"; then
         # Append the cleaned line to the table, preserving its format, and add \hline for row separation
         echo "$clean_line \\\\" >> "$TABLE_FILE"
         echo "    \hline" >> "$TABLE_FILE"
