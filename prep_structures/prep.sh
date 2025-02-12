e8e42ece9afcda4ed9b920fdca51da3fb26e71b3
nayanikadas <nayanikadas0709@gmail.com>
2025-02-12 22:37:50 +0100

    all

--- prep_structures/prep.sh
+++ prep_structures/prep.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # Directory containing PDB files
-pdb_dir="/home/hp/nayanika/github/GPX6/prep_structures/MOUSE/48_47_52_99_54_144_177_74_178_143_87_142_104_102_139_24_181_4_60_107"
+pdb_dir="../../GPX6/prep_structures/MOUSE/level0"
 
 # Loop through each PDB file
 for pdb_file in "$pdb_dir"/*.pdb; do
     base_name=$(basename "$pdb_file" .pdb)
     
-    # Create input file for qprep5
-    cat <<EOF > "${base_name}.inp"
-readlib /home/hp/nayanika/github/GPX6/parameters/qoplsaa.lib
-readlib /home/hp/nayanika/github/GPX6/parameters/GPX.lib
-readprm /home/hp/nayanika/github/GPX6/parameters/qoplsaa_all2.prm
+    # Create input file for qprep5 in the target directory
+    cat <<EOF > "$pdb_dir/${base_name}.inp"
+readlib ../../GPX6/parameters/qoplsaa.lib
+readlib ../../GPX6/parameters/GPX.lib
+readprm ../../GPX6/parameters/qoplsaa_all2.prm
 
 readpdb "$pdb_file"
 
 set solvent_pack 2.7
-boundary sphere 49:SE 25.
-solvate 49:SE 25. grid HOH
+boundary sphere 49:SG 25.
+solvate 49:SG 25. grid HOH
 
-maketop "${base_name}_solvated.top"
-writetop "${base_name}_solvated.top"
-writepdb "${base_name}_solvated.pdb" y
+maketop "$pdb_dir/${base_name}_solvated.top"
+writetop "$pdb_dir/${base_name}_solvated.top"
+writepdb "$pdb_dir/${base_name}_solvated.pdb" y
 
 quit
 EOF
 
-    # Run qprep5 for each PDB
-    qprep5 "${base_name}.inp"
+    # Run qprep5 for each PDB, specifying the input file path within the directory
+    qprep5 "$pdb_dir/${base_name}.inp"
 done
