d836cfc9c92dc68c13eb34afc6f75f274e3ad9cc
nayanikadas <nayanikadas0709@gmail.com>
2025-02-26 12:50:34 +0100

    all

--- analysis/traj.py
+++ analysis/traj.py
@@ -5,7 +5,7 @@ import os
 
 # Set the paths
 pdb_file = "/home/hp/results/MOUSE/level0/D148E/minim/D148E_minim.pdb"
-dcd_path = "/home/hp/results/MOUSE/level0/D148E/test000"
+dcd_path = "/home/hp/results/MOUSE/level0/D148E/product_10_10"
 #dcd_path = "/home/hp/results/MOUSE_OLD/MOUSE/level0/D148E/test5000"
 dcd_files = sorted(glob.glob(os.path.join(dcd_path, "*.dcd")))
 
