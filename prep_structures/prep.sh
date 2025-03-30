349ac1baffea6487222aab9adb2f857ea029702a
nayanikadas <nayanikadas0709@gmail.com>
2025-03-30 16:05:34 +0200

    all

--- prep_structures/prep.sh
+++ prep_structures/prep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Directory containing PDB files
-pdb_dir="../../GPX6/prep_structures/MOUSE/level2"
+pdb_dir="../../GPX6/prep_structures/MOUSE/level3"
 # Create a log file
 log_file="$pdb_dir/solvation_log.txt"
 echo "Starting solvation process at $(date)" > "$log_file"
