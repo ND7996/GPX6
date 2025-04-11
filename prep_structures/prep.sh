466986a56cf8457feccb3f69ccf81cfba1c8c97b
Nayanika Das <nayanikadas0709@gmail.com>
2025-04-11 14:52:27 +0200

    dipole

--- prep_structures/prep.sh
+++ prep_structures/prep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Directory containing PDB files
-pdb_dir="../../GPX6/prep_structures/HUMAN/level3"
+pdb_dir="../../GPX6/prep_structures/MOUSE/level5"
 # Create a log file
 log_file="$pdb_dir/solvation_log.txt"
 echo "Starting solvation process at $(date)" > "$log_file"
