374e30253362a4cc33aa77f1dbe0b62892c40232
Nayanika Das <nayanikadas0709@gmail.com>
2025-04-21 18:15:38 +0200

    all

--- prep_structures/atomnames.py
+++ prep_structures/atomnames.py
@@ -2,7 +2,7 @@ import os
 import re
 
 # Define the base directory containing all PDB files
-base_directory = '/home/hp/nayanika/github/GPX6/prep_structures/HUMAN/level5'
+base_directory = '/home/hp/nayanika/github/GPX6/prep_structures/HUMAN/level6'
 
 def fix_pdb_file(pdb_file):
     print(f"Processing file: {pdb_file}")
