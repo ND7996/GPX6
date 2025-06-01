54df5b8b1a8f0cebd5e52d721b0bba5afab0bb8e
Nayanika Das <nayanikadas0709@gmail.com>
2025-06-01 13:06:38 +0200

    all

--- analysis/optimize.sh
+++ analysis/optimize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input file
-input_file="/home/hp/results/HUMAN/level13/combined_latex_table.tex"
+input_file="/home/hp/results/MOUSE/level17/combined_latex_table.tex"
 
 # Initialize variables
 lowest_value=99999
